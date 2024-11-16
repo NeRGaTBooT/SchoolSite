b0d7f756ef5a097a671200f42b1a9afbdbed76ce
Oleg Galchinsky <64706017+NeRGaTBooT@users.noreply.github.com>
2024-11-16 15:26:31 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
-// Бургер-меню
-const burger = document.getElementById('burger');
-const navMain = document.querySelector('.main_menu');
-const burgerIcon = document.getElementById('burger-icon');
+// // Бургер-меню
+// const burger = document.getElementById('burger');
+// const navMain = document.querySelector('.main_menu');
+// const burgerIcon = document.getElementById('burger-icon');
 
-burger.addEventListener('click', () => {
-    if (navMain.classList.contains('active')) {
-        navMain.classList.remove('active');
-        setTimeout(() => {
-            navMain.style.display = 'none'; // Установка display: none после анимации
-        }, 300); // Длительность анимации
-        burgerIcon.src = 'img/ico/menu.svg'; // Путь к изображению меню
-    } else {
-        navMain.style.display = 'flex'; // Установка display: block при открытии
-        setTimeout(() => {
-            navMain.classList.add('active'); // Добавление класса active после отображения
-        }, 10); // Небольшая задержка для корректной анимации
-        burgerIcon.src = 'img/ico/x.svg'; // Путь к изображению крестика
-    }
+// burger.addEventListener('click', () => {
+//     if (navMain.classList.contains('active')) {
+//         navMain.classList.remove('active');
+//         setTimeout(() => {
+//             navMain.style.display = 'none'; // Установка display: none после анимации
+//         }, 300); // Длительность анимации
+//         burgerIcon.src = 'img/ico/menu.svg'; // Путь к изображению меню
+//     } else {
+//         navMain.style.display = 'flex'; // Установка display: block при открытии
+//         setTimeout(() => {
+//             navMain.classList.add('active'); // Добавление класса active после отображения
+//         }, 10); // Небольшая задержка для корректной анимации
+//         burgerIcon.src = 'img/ico/x.svg'; // Путь к изображению крестика
+//     }
+// });
+
+document.querySelectorAll('.tab-button').forEach(button => {
+    button.addEventListener('click', () => {
+        // Удаляем активный класс у всех кнопок
+        document.querySelectorAll('.tab-button').forEach(btn => btn.classList.remove('active'));
+        // Добавляем активный класс к нажатой кнопке
+        button.classList.add('active');
+        console.log('sdfsdf');
+        // Скрываем все содержимое
+        document.querySelectorAll('.content').forEach(content => content.classList.remove('active'));
+        
+        // Показываем соответствующее содержимое
+        const tabId = button.getAttribute('data-tab');
+        document.getElementById(tabId).classList.add('active');
+    });
 });
+
+
