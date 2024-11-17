3ebebd36234f7c8efe05f64d4839f53ffff2b43c
Oleg Galchinsky <64706017+NeRGaTBooT@users.noreply.github.com>
2024-11-17 21:43:14 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-// // Бургер-меню
-// const burger = document.getElementById('burger');
-// const navMain = document.querySelector('.main_menu');
-// const burgerIcon = document.getElementById('burger-icon');
+// Бургер-меню
+const burger = document.getElementById('burger');
+const navMain = document.querySelector('.main_menu');
+const burgerIcon = document.getElementById('burger-icon');
 
-// burger.addEventListener('click', () => {
-//     if (navMain.classList.contains('active')) {
-//         navMain.classList.remove('active');
-//         setTimeout(() => {
-//             navMain.style.display = 'none'; // Установка display: none после анимации
-//         }, 300); // Длительность анимации
-//         burgerIcon.src = 'img/ico/menu.svg'; // Путь к изображению меню
-//     } else {
-//         navMain.style.display = 'flex'; // Установка display: block при открытии
-//         setTimeout(() => {
-//             navMain.classList.add('active'); // Добавление класса active после отображения
-//         }, 10); // Небольшая задержка для корректной анимации
-//         burgerIcon.src = 'img/ico/x.svg'; // Путь к изображению крестика
-//     }
-// });
+burger.addEventListener('click', () => {
+    if (navMain.classList.contains('active')) {
+        navMain.classList.remove('active');
+        setTimeout(() => {
+            navMain.style.display = 'none'; // Установка display: none после анимации
+        }, 300); // Длительность анимации
+        burgerIcon.src = 'img/icons/menu.svg'; // Путь к изображению меню
+    } else {
+        navMain.style.display = 'flex'; // Установка display: block при открытии
+        setTimeout(() => {
+            navMain.classList.add('active'); // Добавление класса active после отображения
+        }, 10); // Небольшая задержка для корректной анимации
+        burgerIcon.src = 'img/icons/x.svg'; // Путь к изображению крестика
+    }
+});
 
 document.querySelectorAll('.tab-button').forEach(button => {
     button.addEventListener('click', () => {
--- js/slick_init.js
+++ js/slick_init.js
@@ -7,6 +7,15 @@ $(document).ready(function(){
       autoplay: true,
       autoplaySpeed: 2500
     });
-  
-   
+
+    $('.section_slider_rek_wrap').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 2,
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 2500
+    });
+
 });
