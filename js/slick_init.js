2cffe9d8a0ff62cdfe5f0d49fbbc0dcf7576e9fd
Oleg Galchinsky <64706017+NeRGaTBooT@users.noreply.github.com>
2024-11-29 23:01:03 +0300

    Add files via upload

--- js/slick_init.js
+++ js/slick_init.js
@@ -15,7 +15,36 @@ $(document).ready(function(){
       arrows: false,
       dots: true,
       autoplay: true,
-      autoplaySpeed: 2500
+      autoplaySpeed: 2500,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 425,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 320,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+      ]
     });
 
     $('.slider_about_header').slick({
@@ -24,7 +53,8 @@ $(document).ready(function(){
       slidesToScroll: 1,
       arrows: false,
       autoplay: true,
-      autoplaySpeed: 2500
+      autoplaySpeed: 2500,
+      
     });
     
 
